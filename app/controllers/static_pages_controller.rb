2032a15cfb0254883bf8f5328bb13459cd9c731c
Victor Christensen <victor.christensen@cox.net>
2017-06-13 04:18:06 +0000

    firehose track 4, lesson 4

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+# firehose track 4, lesson 4 - file created 12 jun 17
+
+class StaticPagesController < ApplicationController
+    
+    def index
+        
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # next line added 12 jun 17 to hook up routes
+  root "static_pages#index"
 end
