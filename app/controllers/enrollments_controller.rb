8ce212af4b38f39b900edfd5640b73860f3c1aac
Victor Christensen <victor.christensen@cox.net>
2017-06-29 04:28:47 +0000

    firehose track 4, lesson 29 processing payments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -4,27 +4,33 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    current_user.enrollments.create(course: current_course)
+#    current_user.enrollments.create(course: current_course)
 #    redirect_to course_path(current_course)
-    
-    # next 14 lines added 28 jun 17 for accepting payments using stripe, copied/modified from strip.com (lesson 29)
-    
-    # Amount in cents
-    @amount = (current_course.cost * 100).to_i
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]  # stripe token is the value Stripe gives us, which links this 2nd stage of the credit card capturing 
-    )                               # process to the 1st stage. it allows us to reference credit card details for the specific user who 
-                                    # entered their information.
-
-    charge = Stripe::Charge.create(     # this step reaches out to the Stripe servers to capture the funds from our user's bank account
-      customer: customer.id,
-      amount: @amount,
-      description: 'Flixter Premium Content',
-      currency: 'usd'
-    )
+# next line added 28 jun 17 so action to take money not started for free course (lesson 29)
+    if current_course.premium?
+      
+      # next 14 lines added 28 jun 17 for accepting payments using stripe, copied/modified from strip.com (lesson 29)
+      
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
+  
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]  # stripe token is the value Stripe gives us, which links this 2nd stage of the credit card capturing 
+      )                               # process to the 1st stage. it allows us to reference credit card details for the specific user who 
+                                      # entered their information.
+  
+      charge = Stripe::Charge.create(     # this step reaches out to the Stripe servers to capture the funds from our user's bank account
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premium Content',
+        currency: 'usd'
+      )
+    end
     
+# next line moved here 28 jun 17 to prevent users with declined payments to still have access to lessons (lesson 29)
+  current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course)   # moved from def create block 28 jun 17 (lesson 29)
 
   rescue Stripe::CardError => e       # only triggered if Stripe raises an error
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 # firehose track 4, lesson 13 - file created 16 jun 17 for courses
 
 class Course < ApplicationRecord
+  
+# next line added 23 jun 17 - mounts uploader for lesson 22 (challenge)
+  mount_uploader :image, ImageUploader
+    
   belongs_to :user
   has_many :sections
   
@@ -11,7 +15,13 @@ class Course < ApplicationRecord
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
   
-# next line added 23 jun 17 - mounts uploader for lesson 22 (challenge)
-    mount_uploader :image, ImageUploader
+# next 6 lines added 28 jun 17 for bypassing payment info for free course (lesson 29)
+  def free?
+    cost.zero?
+  end
+  
+  def premium?
+    ! free?
+  end
     
 end
